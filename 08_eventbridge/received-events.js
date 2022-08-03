5dabcef6b60549fdd778bf6a612c0a846e7ca848
BourgoisMickael <mickael.bourgois@epitech.eu>
2022-08-03 09:47:46 +0200

    EventBridge testing

---
+++ 08_eventbridge/received-events.js
@@ -0,0 +1,50 @@
+[
+    {
+        version: '0',
+        id: '1e789968-f1d2-426f-db05-e8bca2e199d8',
+        'detail-type': 'CloudFormation Stack Status Change',
+        source: 'aws.cloudformation',
+        account: 'XXXXXXXXXXXX',
+        time: '2022-08-03T07:22:45Z',
+        region: 'eu-west-3',
+        resources: [
+          'arn:aws:cloudformation:eu-west-3:XXXXXXXXXXXX:stack/cf-event-testing/dca9d690-12fc-11ed-acb4-0e99ce9abea2'
+        ],
+        detail: {
+          'stack-id': 'arn:aws:cloudformation:eu-west-3:XXXXXXXXXXXX:stack/cf-event-testing/dca9d690-12fc-11ed-acb4-0e99ce9abea2',
+          'status-details': { status: 'CREATE_COMPLETE', 'status-reason': '' }
+        }
+      },
+      {
+        version: '0',
+        id: 'a952e2a6-392c-1b6d-0f4d-9dbdec50f3b5',
+        'detail-type': 'CloudFormation Stack Status Change',
+        source: 'aws.cloudformation',
+        account: 'XXXXXXXXXXXX',
+        time: '2022-08-03T07:25:22Z',
+        region: 'eu-west-3',
+        resources: [
+          'arn:aws:cloudformation:eu-west-3:XXXXXXXXXXXX:stack/cf-event-testing/dca9d690-12fc-11ed-acb4-0e99ce9abea2'
+        ],
+        detail: {
+          'stack-id': 'arn:aws:cloudformation:eu-west-3:XXXXXXXXXXXX:stack/cf-event-testing/dca9d690-12fc-11ed-acb4-0e99ce9abea2',
+          'status-details': { status: 'UPDATE_COMPLETE', 'status-reason': '' }
+        }
+      },
+      {
+        version: '0',
+        id: 'ff885582-0201-c842-e354-30993b5a907a',
+        'detail-type': 'CloudFormation Stack Status Change',
+        source: 'aws.cloudformation',
+        account: 'XXXXXXXXXXXX',
+        time: '2022-08-03T07:26:32Z',
+        region: 'eu-west-3',
+        resources: [
+          'arn:aws:cloudformation:eu-west-3:XXXXXXXXXXXX:stack/cf-event-testing/dca9d690-12fc-11ed-acb4-0e99ce9abea2'
+        ],
+        detail: {
+          'stack-id': 'arn:aws:cloudformation:eu-west-3:XXXXXXXXXXXX:stack/cf-event-testing/dca9d690-12fc-11ed-acb4-0e99ce9abea2',
+          'status-details': { status: 'DELETE_COMPLETE', 'status-reason': '' }
+        }
+      }
+]
